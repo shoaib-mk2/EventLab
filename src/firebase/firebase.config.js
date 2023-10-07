7f3bc113fb965f6834ace35bc912f8bb93694cb2
shoaib-mk2 <shoaibchowdhury707@gmail.com>
2023-10-07 21:29:17 +0600

    installed React Router, tailwind CSS, daisyUI and firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBvCryrbEEypvHvIX7SVtsA1yh4xfJbD3Q",
+    authDomain: "react-event-management-site.firebaseapp.com",
+    projectId: "react-event-management-site",
+    storageBucket: "react-event-management-site.appspot.com",
+    messagingSenderId: "908179937386",
+    appId: "1:908179937386:web:693daecf12008030351213"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
